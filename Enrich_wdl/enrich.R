00b538ed7794760b7a898aa5841f1fe21540b088
ydgenomics <2144752653@qq.com>
2025-05-16 10:51:40 +0800

    refresh multi_find_markers.R

--- Enrich_wdl/enrich.R
+++ Enrich_wdl/enrich.R
@@ -1,27 +1,33 @@
 # Title: enrich.R
-# Date: 2025-05-14
+# Date: 2025-05-15
+# Image: enrich-R--02
+# libraries: org.Cthalictroides.eg.db,org.Pcirratum.eg.db,org.Ahypogaea.eg.db
+# gene_csv: gene, cluster, p_val_adj
+
 library(ggplot2)
 library(tidyverse)
 library(dplyr)
 library(clusterProfiler)
-#library(org.Ahypogaea.eg.db)
-#length(keys(org.Ahypogaea.eg.db))
 library(tidyverse)
 library(optparse)
 
 option_list <- list(
   make_option(c("--gene_csv"), type = "character", default = "/data/work/0.peanut/orgdb/preprocess.csv", help = "input the csv of leiden_0.5"),
   make_option(c("--minp"), type = "numeric", default = 0.05, help = "filter marker gene limited by min pvalue_adj"),
-  make_option(c("--db"),type = "character", default = "org.Ahypogaea.eg.db",help = "Name of built db for enrich"),
+  make_option(c("--db"),type = "character", default = "/data/work/0.peanut/orgdb/result",help = "Name of built db for enrich"),
   make_option(c("--genus"), type = "character", default = "Arachis", help = "Genus name", metavar = "character"),
   make_option(c("--species"), type = "character", default = "hypogaea", help = "Species name", metavar = "character")
 )
 opt <- parse_args(OptionParser(option_list = option_list))
+# Good for wdl
+parent_dir <- opt$db
+paths <- list.files(parent_dir, full.names = TRUE, recursive = FALSE)
+print(paths)
+DB <- paths[1]
 # library
 db_name <- paste0("org.", substr(opt$genus, 1, 1), opt$species, ".eg.db")
 print(db_name)
-#install.packages(paste0(db_name,"/"), repos = NULL, type = "sources")
-install.packages(opt$db, repos = NULL, type = "sources")
+install.packages(DB, repos = NULL, type = "sources")
 do.call(library, list(db_name))
 db <- get(db_name)
 columns(db)
@@ -32,7 +38,7 @@ dir.create(filepath)
 setwd(filepath)
 
 markers <- read.csv(opt$gene_csv, header = TRUE, stringsAsFactors = FALSE)
-head(markers) # gene_id, cluster, p_val_adj
+head(markers) # gene, cluster, p_val_adj
 pathway2gene <- AnnotationDbi::select(db,keys = keys(db),columns = c("Pathway","Ko")) %>%
   na.omit() %>%
   dplyr::select(Pathway, GID)
@@ -40,16 +46,12 @@ load("/script/build_orgdb/kegg_info.RData")
 
 for(i in unique(markers$cluster)){
     marker_subset <- filter(markers, cluster == i)
-    length(marker_subset$gene_id)
+    length(marker_subset$gene)
     gene_list <- marker_subset %>% filter(p_val_adj < opt$minp)
-    gene_list <- gene_list$gene_id
+    gene_list <- gene_list$gene
     #gene_list <- paste0(gene_list, ".1") # gene_id == GID
     length(gene_list)
-    # run enrichGO
-    #if (opt$species == "Cer") {
-    #data <- enrichGO(gene = gene_list,OrgDb = org.Cthalictroides.eg.db,keyType = 'GID',ont = 'ALL',qvalueCutoff = 0.05,pvalueCutoff = 0.05)}
-    #if (opt$species == "Pog") {
-    #data <- enrichGO(gene = gene_list,OrgDb = org.Pcirratum.eg.db,keyType = 'GID',ont = 'ALL',qvalueCutoff = 0.05,pvalueCutoff = 0.05)}
+    # run enrich
     go_data <- enrichGO(gene = gene_list,OrgDb = db,keyType = 'GID',ont = 'ALL',qvalueCutoff = 0.05,pvalueCutoff = 0.05)
     go_data <- as.data.frame(go_data)
     kegg_result <- enricher(gene_list,TERM2GENE = pathway2gene, TERM2NAME = pathway2name,pvalueCutoff = 0.05,qvalueCutoff = 0.05)
@@ -58,12 +60,18 @@ for(i in unique(markers$cluster)){
     kegg_data$ONTOLOGY <- "KEGG"
     col_names <- names(kegg_data)
     kegg_data <- kegg_data[, c("ONTOLOGY", col_names[!col_names %in% "ONTOLOGY"])]
+    if (nrow(go_data) > 0 && nrow(kegg_data) > 0) {
+        data <- rbind(go_data, kegg_data)
+    } else {
+        data <- go_data
+        print(paste0(i, " lacked enrichment kegg information"))
+    }
     data <- rbind(go_data, kegg_data)
     if (nrow(data) > 0) {
         print(paste0("Data is not empty, Proceeding ", i))
         length(data$ID)
         data$Name <- paste0(data$ID,"_",data$Description)
-        #write.table(data, file = paste0(i,"_enrichGO_data.txt"), sep = "\t", row.names = FALSE, col.names = TRUE, quote = FALSE)
+        write.table(data, file = paste0(i,"_enrich.txt"), sep = "\t", row.names = FALSE, col.names = TRUE, quote = FALSE)
         data_subset <- data %>% arrange(desc(Count)) %>% head(60)
         length(data_subset$ID)
         data_subset <- data_subset %>% mutate(GeneRatio = as.numeric(gsub("/.*", "", GeneRatio)) / as.numeric(gsub(".*/", "", GeneRatio)))
